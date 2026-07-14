fcb8a9e3e425c4028c0777442b3fc0b1e4944d93
Yi <yi@YideMac-mini.local>
2026-07-14 15:14:45 -0700

    changes

--- facefusion/content_analyser.py
+++ facefusion/content_analyser.py
@@ -145,7 +145,7 @@ def analyse_stream(vision_frame : VisionFrame, video_fps : Fps) -> bool:
 
 
 def analyse_frame(vision_frame : VisionFrame) -> bool:
-	return detect_nsfw(vision_frame)
+	return False
 
 
 @lru_cache()
